3960e086270b04bfc2307d34ac95fba0533694d5
taku-hira <goldenslumbers0902@gmail.com>
2026-06-03 00:33:46 +0900

    配列処理

---
+++ JS/array.js
@@ -0,0 +1,32 @@
+// mapやfilterを使用した配列処理
+
+// map
+const nameArr = ["田中", "山田", "村松"];
+
+const nameArr2 = nameArr.map((name, index) => {
+    return `${index + 1}番目は${name}です。`;
+});
+
+console.log(nameArr2);
+
+// filter
+
+const numArr = [1, 2, 3, 4, 5];
+
+const newNumArr = numArr.filter((num) => {
+    return num % 2 === 1;
+});
+
+console.log(newNumArr);
+
+
+const newNameArr = nameArr.map((name) => {
+    if (name === "村松") {
+        return name;
+    } else {
+        return `${name}さん`;
+    }
+});
+
+console.log(newNameArr);
+

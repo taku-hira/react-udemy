e685a8fc24f081fcdff67bb2f2a9982fe5a58825
taku-hira <goldenslumbers0902@gmail.com>
2026-06-05 23:43:50 +0900

    props state useEffect

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
 import { ColorfulMessage } from './components/ColorfulMessage'
 
 function App() {
+  console.log('Appがレンダリングされました')
   // Props
+  const [num, setNum] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(true);
+  // state
+  const onClickCountUp = () => {
+    setNum((prev) => prev + 1);
+  }
+
+  const onClickToggle = () => {
+    setIsShowFace(!isShowFace);
+  }
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      isShowFace || setIsShowFace(true);
+    } else {
+      isShowFace && setIsShowFace(false);
+    }
+   }, [num])
+
+  
   return (
     <div>
       <ColorfulMessage color="blue">
@@ -11,7 +33,10 @@ function App() {
       元気です
       </ColorfulMessage>
       { console.log('コンソールに出力') }
-      <button onClick={ () => alert('ボタンがクリックされました！') }>ボタン</button>
+      <button onClick={onClickCountUp}>ボタン</button>
+      <p>{num}</p>
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowFace && <p>(^^)</p>}
     </div>
   )
 }
